a3d981e0f15fbd255ff5bd36d78ecda54cb43d50
MrAnyx <1ptitigre@gmail.com>
2023-09-16 22:40:18 +0000

    feat(core) Added basic structure

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
     <h1>Hello World</h1>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import axios from "@/plugins/axios";
+
+axios.get("/auth/login");
+</script>
 
 <style scoped lang="scss">
 h1 {
---
+++ src/plugins/axios.ts
@@ -0,0 +1,25 @@
+import axios, { InternalAxiosRequestConfig } from "axios";
+import { getToken } from "@/services/authentication";
+
+// Set default axios parameters
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+// Catch request and set Authorization header
+instance.interceptors.request.use(
+    (request: InternalAxiosRequestConfig) => {
+        const token = getToken();
+
+        if (token) {
+            request.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return request;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
+
+export default instance;
