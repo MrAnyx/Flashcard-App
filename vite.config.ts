e1ca9a067892843aa5ce40e71d6ff5043ea9423c
MrAnyx <1ptitigre@gmail.com>
2023-11-30 22:30:33 +0000

    fix(core) Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
+    server: {
+        port: 5000,
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
