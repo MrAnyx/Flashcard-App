14af49330aaa4db9e6b3a49c9c8d423a17d64150
MrAnyx <1ptitigre@gmail.com>
2024-01-16 22:17:07 +0000

    feat(login) Added form validation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,3 @@
 <template>
     <RouterView />
 </template>
-
-<script setup lang="ts">
-import axios from "@/plugins/axios";
-
-axios.post("/auth/login", {
-    identifier: "user",
-    password: "password",
-});
-</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,36 +2,48 @@
     <div id="container">
         <Card class="w-11 md:w-8 lg:w-6 xl:w-4">
             <template #content>
-                <div class="text-center mb-5">
-                    <RouterLink to="/">
-                        <img src="../assets/images/logo.svg" alt="Image" height="50" class="mb-3" />
-                    </RouterLink>
-                    <div class="text-900 text-3xl font-medium mb-3">Welcome Back</div>
-                    <span class="text-600 font-medium line-height-3">Don't have an account?</span>
-                    <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
-                </div>
-
-                <div>
-                    <div class="flex flex-column gap-4">
-                        <div class="flex flex-column gap-2">
-                            <label for="username">Email or username</label>
-                            <InputText id="username" v-model="username" />
-                        </div>
-                        <div class="flex flex-column gap-2">
-                            <label for="password1" placeholder="Email address">Password</label>
-                            <Password id="password1" v-model="password" toggle-mask :feedback="false" input-class="w-full" />
-                        </div>
-                        <div class="flex align-items-center justify-content-between">
-                            <div class="flex align-items-center">
-                                <Checkbox input-id="rememberme1" :binary="true" v-model="rememberMe" class="mr-2"></Checkbox>
-                                <label for="rememberme1">Remember me</label>
-                            </div>
-                            <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot password?</a>
-                        </div>
+                <form @submit.prevent="onSubmit">
+                    <div class="text-center mb-5">
+                        <RouterLink to="/">
+                            <img src="../assets/images/logo.svg" alt="Image" height="50" class="mb-3" />
+                        </RouterLink>
+                        <div class="text-900 text-3xl font-medium mb-3">Welcome Back</div>
+                        <span class="text-600 font-medium line-height-3">Don't have an account?</span>
+                        <a class="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
                     </div>
 
-                    <Button label="Sign In" icon="pi pi-user" class="w-full mt-6"></Button>
-                </div>
+                    <div>
+                        <div class="flex flex-column gap-4">
+                            <!-- <InlineMessage v-if="$v.$error" severity="error">Error Message</InlineMessage> -->
+                            <div class="flex flex-column gap-2">
+                                <label for="username">Email or username</label>
+                                <InputText id="username" :disabled="isFormLoading" :class="{ 'p-invalid': $v.username.$error }" v-model="formInputs.username" />
+                                <small class="p-error" v-if="$v.username.$error">{{ $v.username.$errors[0].$message }}</small>
+                            </div>
+                            <div class="flex flex-column gap-2">
+                                <label for="password1" placeholder="Email address">Password</label>
+                                <Password
+                                    id="password1"
+                                    :disabled="isFormLoading"
+                                    v-model="formInputs.password"
+                                    toggle-mask
+                                    :feedback="false"
+                                    :input-class="['w-full', { 'p-invalid': $v.password.$error }]"
+                                />
+                                <small class="p-error" v-if="$v.password.$error">{{ $v.password.$errors[0].$message }}</small>
+                            </div>
+                            <div class="flex align-items-center justify-content-between">
+                                <div class="flex align-items-center">
+                                    <Checkbox input-id="rememberme1" :disabled="isFormLoading" :binary="true" v-model="formInputs.rememberMe" class="mr-2"></Checkbox>
+                                    <label for="rememberme1">Remember me</label>
+                                </div>
+                                <a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot password?</a>
+                            </div>
+                        </div>
+
+                        <Button type="submit" label="Sign In" icon="pi pi-user" class="w-full mt-6" :loading="isFormLoading"></Button>
+                    </div>
+                </form>
             </template>
         </Card>
     </div>
@@ -42,13 +54,42 @@ import InputText from "primevue/inputtext";
 import Checkbox from "primevue/checkbox";
 import Button from "primevue/button";
 import Card from "primevue/card";
-import { ref } from "vue";
+import { computed, reactive, ref } from "vue";
 import { RouterLink } from "vue-router";
 import Password from "primevue/password";
+import { useVuelidate } from "@vuelidate/core";
+import { required } from "@vuelidate/validators";
+import axios from "@/plugins/axios";
 
-const rememberMe = ref(true);
-const username = ref("");
-const password = ref("");
+const isFormLoading = ref(false);
+
+const formInputs = reactive({
+    rememberMe: true,
+    username: "",
+    password: "",
+});
+
+const formInputsRules = computed(() => ({
+    username: {
+        required,
+    },
+    password: {
+        required,
+    },
+}));
+
+const $v = useVuelidate(formInputsRules, formInputs);
+
+const onSubmit = async () => {
+    isFormLoading.value = true;
+    const isValid = await $v.value.$validate();
+    if (!isValid) return;
+
+    await axios.post("/auth/login", {
+        identifier: formInputs.username,
+        password: formInputs.password,
+    });
+};
 </script>
 
 <style lang="scss">
