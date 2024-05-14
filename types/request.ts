58a484443e594daced3af9d56550fdd238bfbee6
MrAnyx <1ptitigre@gmail.com>
2024-05-14 21:51:45 +0000

    feat(request) Added types on requests

--- types/request.ts
+++ types/request.ts
@@ -1,19 +1,19 @@
 export type JsonStatus = "invalid" | "valid";
 
 export type JsonPagination = {
-    total: number,
-    count: number,
-    offset: number,
-    items_per_page: number,
-    total_pages: number,
-    current_page: number,
-    has_next_page: boolean,
-    has_previous_page: boolean
-}
+    total: number;
+    count: number;
+    offset: number;
+    items_per_page: number;
+    total_pages: number;
+    current_page: number;
+    has_next_page: boolean;
+    has_previous_page: boolean;
+};
 
 export type JsonStandard<TData> = {
-    "@timestamp": string,
-    "@pagination": JsonPagination | null,
-    "@status": string,
-    data: TData
-}
+    "@timestamp": string;
+    "@pagination": JsonPagination | null;
+    "@status": JsonStatus;
+    data: TData;
+};
