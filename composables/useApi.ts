991ec037a744e615d33215fac3c16c1317548c5b
MrAnyx <1ptitigre@gmail.com>
2024-06-08 19:37:17 +0000

    fix(composable) Removed retry feature on useApi composable

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -4,6 +4,7 @@ import type { JsonStandard } from "~/types/request";
 export function useApi<T>(url: string | (() => string), options: UseFetchOptions<JsonStandard<T>> = {}) {
     return useFetch<JsonStandard<T>>(url, {
         ...options,
+        retry: false,
         $fetch: useNuxtApp().$apiFetch
     });
 }
