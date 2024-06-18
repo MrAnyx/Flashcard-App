861060973327d20f9b956a83b3f0aa26c52f43fd
MrAnyx <1ptitigre@gmail.com>
2024-06-18 20:24:02 +0000

    doc(pinia) Add comment on cookie performances

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,6 +24,7 @@ export default defineNuxtConfig({
         cookieOptions: {
             sameSite: "strict"
         },
+        // Cookie has better performances
         storage: "localStorage"
     },
     router: {
