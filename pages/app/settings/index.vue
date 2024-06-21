6813129e58d051acd24781c08f1e94c53699b49c
MrAnyx <1ptitigre@gmail.com>
2024-06-21 22:24:14 +0000

    feat(settings) Add account deletion zone + actual email and username in inputs

--- pages/app/settings/index.vue
+++ pages/app/settings/index.vue
@@ -6,29 +6,38 @@
             </h2>
             <span class="text-gray-400 text-sm">Customize your personal informations</span>
         </div>
-        <section>
+        <section class="flex flex-col gap-y-6">
             <div class="grid grid-cols-1 gap-3 lg:grid-cols-2 items-center">
                 <div>
                     <h4>Email</h4>
                     <span class="text-gray-400 text-sm">Used to login and receive notifications</span>
                 </div>
                 <UInput
-                    value="example@mail.com"
+                    v-model="profileState.email"
                     type="email"
                     icon="i-heroicons-envelope"
                 />
             </div>
-            <div class="grid grid-cols-1 gap-3 lg:grid-cols-2 items-center mt-6">
+            <div class="grid grid-cols-1 gap-3 lg:grid-cols-2 items-center">
                 <div>
                     <h4>Username</h4>
                     <span class="text-gray-400 text-sm">Your unique identifier used to login</span>
                 </div>
                 <UInput
-                    value="MrAnyx"
+                    v-model="profileState.username"
                     icon="i-heroicons-user"
                 />
             </div>
-            <div class="grid grid-cols-1 gap-3 lg:grid-cols-2 items-center mt-6">
+        </section>
+        <UDivider />
+        <div>
+            <h2 class="font-medium text-gray-200 text-xl">
+                Security
+            </h2>
+            <span class="text-gray-400 text-sm">Customize your personal informations</span>
+        </div>
+        <section class="flex flex-col gap-y-6">
+            <div class="grid grid-cols-1 gap-3 lg:grid-cols-2 items-center">
                 <div>
                     <h4>Password</h4>
                     <span class="text-gray-400 text-sm">Used to securely login to your account</span>
@@ -47,56 +56,21 @@
             </div>
         </section>
         <UDivider />
-        <div>
-            <h2 class="font-medium text-gray-200 text-xl">
-                My profile
-            </h2>
-            <span class="text-gray-400 text-sm">Customize your personal informations</span>
-        </div>
-        <section>
-            <div class="grid grid-cols-1 gap-3 lg:grid-cols-2 items-center">
-                <div>
-                    <h4>Email</h4>
-                    <span class="text-gray-400 text-sm">Used to login and receive notifications</span>
-                </div>
-                <UInput
-                    value="example@mail.com"
-                    type="email"
-                    icon="i-heroicons-envelope"
-                />
-            </div>
-            <div class="grid grid-cols-1 gap-3 lg:grid-cols-2 items-center mt-6">
-                <div>
-                    <h4>Username</h4>
-                    <span class="text-gray-400 text-sm">Your unique identifier used to login</span>
-                </div>
-                <UInput
-                    value="MrAnyx"
-                    icon="i-heroicons-user"
-                />
-            </div>
-            <div class="grid grid-cols-1 gap-3 lg:grid-cols-2 items-center mt-6">
-                <div>
-                    <h4>Password</h4>
-                    <span class="text-gray-400 text-sm">Used to securely login to your account</span>
-                </div>
-                <div>
-                    <UInput
-                        icon="i-heroicons-lock-closed"
-                        placeholder="Current password"
-                        class="mb-2"
-                    />
-                    <UInput
-                        icon="i-heroicons-lock-closed"
-                        placeholder="New password"
-                    />
-                </div>
-            </div>
-        </section>
+        <UAlert
+            title="Danger zone"
+            icon="i-heroicons-exclamation-triangle"
+            description="Permanently delete your account with all your data"
+            color="red"
+            variant="subtle"
+            :actions="deleteAccountActions"
+        />
     </div>
 </template>
 
 <script setup lang="ts">
+import { z } from "zod";
+import type { AlertAction } from "#ui/types";
+
 definePageMeta({
     alias: "/app/settings/account",
     name: "settings-account"
@@ -105,4 +79,29 @@ definePageMeta({
 useHead({
     title: "Account"
 });
+
+const authStore = useAuthStore();
+
+// const schema = z.object({
+//     username: z.string()
+//         .min(1, "Identifier can not be blank")
+//         .max(180, "Identifier is too long"),
+//     email: z.string()
+//         .min(1, "Password can not be blank")
+// });
+
+const profileState = reactive({
+    username: authStore.user!.username,
+    email: authStore.user!.email
+});
+
+const deleteAccountActions: AlertAction[] = [
+    {
+        label: "Delete my account",
+        icon: "i-heroicons-trash",
+        color: "red",
+        variant: "solid",
+        click: () => {}
+    },
+];
 </script>
