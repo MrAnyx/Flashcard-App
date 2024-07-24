8cbe38164d4d96ab83563a0b7a25a0b2e3adfa34
MrAnyx <1ptitigre@gmail.com>
2024-07-24 18:13:17 +0000

    feat(repository) Add getMe route

--- repositories/UserRepository.ts
+++ repositories/UserRepository.ts
@@ -1,4 +1,5 @@
 import { AbstractRepository } from "./AbstractRepository";
+import type { User } from "~/types/entity";
 import type { JsonStandard } from "~/types/request";
 
 export class UserRepository extends AbstractRepository
@@ -7,6 +8,13 @@ export class UserRepository extends AbstractRepository
     {
         return this.fetch<JsonStandard<null>>(`/users/me`, {
             method: "DELETE"
+        });
+    };
+
+    async getMe()
+    {
+        return this.fetch<JsonStandard<User>>(`/users/me`, {
+            method: "GET"
         });
     };
 }
