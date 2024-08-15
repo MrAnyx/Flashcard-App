aee98a2cbd24da56724709a8647f0b11ce3e12ff
MrAnyx <1ptitigre@gmail.com>
2024-08-15 17:36:17 +0200

    feat(practice) Update practice page

--- pages/app/practice.vue
+++ pages/app/practice.vue
@@ -11,8 +11,11 @@
                         <h2 class="text-2xl text-gray-700 dark:text-gray-300">
                             Welcome back MrAnyx
                         </h2>
-                        <p class="text-gray-500 dark:text-gray-400">
+                        <p class="text-gray-500 dark:text-gray-400 text-sm">
                             {{ DateTime.now().setLocale('fr').toFormat(' DDDD') }}
+                        </p>
+                        <p class="text-gray-500 dark:text-gray-400">
+                            Boost your skills with your flashcards! Review in a flash and master your knowledge in no time. Each practice session brings you one step closer to your goals. So, are you ready to take on the challenge?
                         </p>
 
                         <div class="flex gap-x-3">
@@ -29,125 +32,129 @@
                         </div>
                     </div>
                 </UCard>
-                <!-- <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4">
-                    <template v-if="provider.loading">
-                        <USkeleton
-                            v-for="i in 4"
-                            :key="i"
-                            class="h-[84px]"
-                        />
-                    </template>
-                    <template v-else>
-                        <UCard
-                            class="h-full"
-                            :ui="{ body: { padding: 'p-3 sm:p-4' } }"
-                        >
-                            <div class="flex items-center space-x-4">
-                                <UIcon
-                                    name="i-tabler-folder"
-                                    class="dark:bg-gray-300 bg-gray-500 text-2xl shrink-0"
-                                />
 
-                                <div class="flex-1 flex flex-col min-w-0 text-left">
-                                    <span class="truncate dark:text-gray-300 text-gray-500">Total topics</span>
-                                    <span
-                                        class="text-xl dark:text-gray-300 text-gray-700 truncate"
-                                    >
-                                        {{ formatNumber(topicStore.total) }}
-                                    </span>
-                                </div>
-                            </div>
-                        </UCard>
-                        <UCard
-                            class="h-full"
-                            :ui="{ body: { padding: 'p-3 sm:p-4' } }"
-                        >
-                            <div class="flex items-center space-x-4">
-                                <UIcon
-                                    name="i-tabler-color-swatch"
-                                    class="dark:bg-gray-300 bg-gray-500 text-2xl shrink-0"
-                                />
+                <UDivider />
 
-                                <div class="flex-1 flex flex-col min-w-0 text-left">
-                                    <span class="truncate dark:text-gray-300 text-gray-500">Total units</span>
-                                    <span
-                                        class="text-xl dark:text-gray-300 text-gray-700 truncate"
-                                    >
-                                        {{ formatNumber(unitStore.total) }}
-                                    </span>
-                                </div>
-                            </div>
-                        </UCard>
-                        <UCard
-                            class="h-full"
-                            :ui="{ body: { padding: 'p-3 sm:p-4' } }"
-                        >
-                            <div class="flex items-center space-x-4">
-                                <UIcon
-                                    name="i-tabler-cards"
-                                    class="dark:bg-gray-300 bg-gray-500 text-2xl shrink-0"
-                                />
+                <USelectMenu
+                    v-model="selectedPeriod"
+                    :options="periods"
+                    class="w-32"
+                />
 
-                                <div class="flex-1 flex flex-col min-w-0 text-left">
-                                    <span class="truncate dark:text-gray-300 text-gray-500">Total flashcards</span>
-                                    <span
-                                        class="text-xl dark:text-gray-300 text-gray-700 truncate"
-                                    >
-                                        {{ formatNumber(flashcardStore.total) }}
-                                    </span>
-                                </div>
-                            </div>
-                        </UCard><UCard
-                            class="h-full"
-                            :ui="{ body: { padding: 'p-3 sm:p-4' } }"
-                        >
-                            <div class="flex items-center space-x-4">
-                                <UIcon
-                                    name="i-tabler-calendar"
-                                    class="dark:bg-gray-300 bg-gray-500 text-2xl shrink-0"
-                                />
+                <div class="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4">
+                    <UCard
+                        class="h-full"
+                        :ui="{ body: { padding: 'p-3 sm:p-4' } }"
+                    >
+                        <div class="flex items-center space-x-4">
+                            <UIcon
+                                name="i-tabler-calendar"
+                                class="dark:bg-gray-300 bg-gray-500 text-2xl shrink-0"
+                            />
 
-                                <div class="flex-1 flex flex-col min-w-0 text-left">
-                                    <span class="truncate dark:text-gray-300 text-gray-500">Total reviews</span>
-                                    <span
-                                        class="text-xl dark:text-gray-300 text-gray-700 truncate"
+                            <div class="flex-1 flex flex-col min-w-0 text-left">
+                                <span class="truncate dark:text-gray-300 text-gray-500">Total reviews</span>
+                                <span class="text-xl dark:text-gray-300 text-gray-700 truncate inline-flex items-center gap-x-2">
+                                    {{ formatNumber(23) }}
+                                    <UTooltip
+                                        :text="`+${formatNumber(23)}% from last period`"
                                     >
-                                        {{ formatNumber(reviewStore.total) }}
-                                    </span>
-                                </div>
+                                        <UIcon
+                                            name="i-tabler-trending-up"
+                                            class="text-green-500"
+                                        />
+                                    </UTooltip>
+                                </span>
                             </div>
-                        </UCard>
-                    </template>
+                        </div>
+                    </UCard>
+                    <UCard
+                        class="h-full"
+                        :ui="{ body: { padding: 'p-3 sm:p-4' } }"
+                    >
+                        <div class="flex items-center space-x-4">
+                            <UIcon
+                                name="i-tabler-calendar"
+                                class="dark:bg-gray-300 bg-gray-500 text-2xl shrink-0"
+                            />
+
+                            <div class="flex-1 flex flex-col min-w-0 text-left">
+                                <span class="truncate dark:text-gray-300 text-gray-500">Correct answers</span>
+                                <span class="text-xl dark:text-gray-300 text-gray-700 truncate inline-flex items-center gap-x-2">
+                                    {{ formatNumber(16) }}
+                                    <UTooltip
+                                        :text="`+${formatNumber(23)}% from last period`"
+                                    >
+                                        <UIcon
+                                            name="i-tabler-trending-up"
+                                            class="text-green-500"
+                                        />
+                                    </UTooltip>
+                                </span>
+                            </div>
+                        </div>
+                    </UCard>
+                    <UCard
+                        class="h-full"
+                        :ui="{ body: { padding: 'p-3 sm:p-4' } }"
+                    >
+                        <div class="flex items-center space-x-4">
+                            <UIcon
+                                name="i-tabler-calendar"
+                                class="dark:bg-gray-300 bg-gray-500 text-2xl shrink-0"
+                            />
+
+                            <div class="flex-1 flex flex-col min-w-0 text-left">
+                                <span class="truncate dark:text-gray-300 text-gray-500">Average answer</span>
+                                <span class="text-xl dark:text-gray-300 text-gray-700 truncate inline-flex items-center gap-x-2">
+                                    {{ formatNumber(3.5) }}
+                                    <UTooltip
+                                        :text="`-${formatNumber(23)}% from last period`"
+                                    >
+                                        <UIcon
+                                            name="i-tabler-trending-down"
+                                            class="text-red-500"
+                                        />
+                                    </UTooltip>
+                                </span>
+                            </div>
+                        </div>
+                    </UCard>
+                    <UCard
+                        class="h-full"
+                        :ui="{ body: { padding: 'p-3 sm:p-4' } }"
+                    >
+                        <div class="flex items-center space-x-4">
+                            <UIcon
+                                name="i-tabler-calendar"
+                                class="dark:bg-gray-300 bg-gray-500 text-2xl shrink-0"
+                            />
+
+                            <div class="flex-1 flex flex-col min-w-0 text-left">
+                                <span class="truncate dark:text-gray-300 text-gray-500">Correct flashcards</span>
+                                <span class="text-xl dark:text-gray-300 text-gray-700 truncate inline-flex items-center gap-x-2">
+                                    {{ formatNumber(98) }}
+                                    <UTooltip
+                                        :text="`+${formatNumber(23)}% from last period`"
+                                    >
+                                        <UIcon
+                                            name="i-tabler-trending-up"
+                                            class="text-green-500"
+                                        />
+                                    </UTooltip>
+                                </span>
+                            </div>
+                        </div>
+                    </UCard>
                 </div>
-                <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-                    <template v-if="provider.loading">
-                        <USkeleton
-                            v-for="i in 2"
-                            :key="i"
-                            class="h-[84px]"
-                        />
-                    </template>
-                    <template v-else>
-                        <UCard
-                            :ui="{ body: { padding: '!p-0' } }"
-                        >
-                            <UTable
-                                :rows="people"
-                                :columns="tableColumns"
-                            />
-                        </UCard>
-                        <UCard
-                            :ui="{ body: { padding: '!p-0' } }"
-                        >
-                            <UTable
-                                :rows="people"
-                                :columns="tableColumns"
-                            />
-                        </UCard>
-                    </template>
-                </div> -->
 
-                <UCard class="w-full">
+                <UCard
+                    class="w-full"
+                    :ui="{ header: { padding: 'p-3' } }"
+                >
+                    <template #header>
+                        Number of reviews
+                    </template>
                     <GraphLine />
                 </UCard>
             </section>
@@ -166,4 +173,7 @@ definePageMeta({
 useHead({
     title: "Practice"
 });
+
+const periods = ["Today", "Last 7 days", "Last 30 days"];
+const selectedPeriod = ref(periods[0]);
 </script>
