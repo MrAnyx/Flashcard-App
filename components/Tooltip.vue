a4459815b12f70b540702581158ca8ad91f44ff7
MrAnyx <1ptitigre@gmail.com>
2024-09-22 20:00:21 +0000

    feat(tooltip) Create custom tooltip component from the UPopover component to allow mobile click

---
+++ components/Tooltip.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <UPopover :mode="props.activation">
+        <template #panel="{ close }">
+            <slot name="content" :close="close">
+                <div class="p-2 md:p-2 max-w-sm md:max-w-lg">
+                    <p
+                        class="text-sm text-gray-600 dark:text-gray-300"
+                        :class="{ truncate: props.truncate }"
+                    >
+                        {{ props.text }}
+                    </p>
+                </div>
+            </slot>
+        </template>
+        <div :class="{ '*:cursor-help': props.help }">
+            <slot />
+        </div>
+    </UPopover>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+    text?: string;
+    activation: "hover" | "click";
+    truncate?: boolean;
+    help?: boolean;
+}>();
+</script>
