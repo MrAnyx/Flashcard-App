19b93fdefe15e7fe707ce5be7e74edf35b2cfcc0
MrAnyx <1ptitigre@gmail.com>
2024-12-15 17:34:29 +0000

    feat(settings) Add general setting page

--- pages/app/settings/general.vue
+++ pages/app/settings/general.vue
@@ -1,19 +1,125 @@
 <template>
-    <div>
-        general
+    <div class="flex flex-col space-y-8">
+        <HeaderWithCaption
+            title="Pagination"
+            caption="Customize how requests are made"
+        />
+        <section class="flex flex-col gap-y-6">
+            <SettingsParameterEntry
+                label="Items per page"
+                caption="Specify the number of items per page"
+            >
+                <USelectMenu
+                    v-model="itemsPerPage"
+                    :options="itemsPerPageObject"
+                    value-attribute="value"
+                    option-attribute="label"
+                    size="md"
+                    @change="updatePagination"
+                />
+            </SettingsParameterEntry>
+        </section>
+
+        <UDivider />
+
+        <HeaderWithCaption
+            title="Session"
+            caption="Customize your session"
+        />
+        <section class="flex flex-col gap-y-6">
+            <SettingsParameterEntry
+                label="Flashcards per session"
+                caption="Specify the number of flashcards per session"
+            >
+                <UInput
+                    v-model="flashcardsPerSession"
+                    type="number"
+                    size="md"
+                    @change="updateFlashcardsPerSession"
+                />
+            </SettingsParameterEntry>
+
+            <SettingsParameterEntry
+                label="Display session introduction"
+                caption="Show the introduction modal before starting a new session"
+            >
+                <UToggle
+                    v-model="showSessionIntroduction"
+                    @change="updateShowSessionIntroduction"
+                />
+            </SettingsParameterEntry>
+        </section>
+
+        <UDivider />
+
+        <HeaderWithCaption
+            title="Data collection"
+            caption="Manage your collection"
+        />
+        <section>
+            <SettingsParameterEntry
+                label="Reset your reviews"
+                caption="Reset your flashcards state and go bask as you just created them"
+            >
+                <UButton
+                    class="self-start justify-self-start"
+                    label="Erase"
+                    color="red"
+                    @click="eraseReviews"
+                />
+            </SettingsParameterEntry>
+        </section>
+
+        <UDivider />
     </div>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+import { ModalConfirm } from "#components";
+
 definePageMeta({
-    name: "settings-general",
+    name: "settings-general"
 });
 
 useHead({
     title: "General"
 });
+
+const authStore = useAuthStore();
+const repository = useRepository();
+const modal = useModal();
+
+const itemsPerPageOptions = [25, 50, 100, 200, 500];
+const itemsPerPageObject = itemsPerPageOptions.map((o) =>
+{
+    return {
+        label: o.toString(),
+        value: o
+    };
+});
+const itemsPerPage = ref(authStore.getSetting<number>("items_per_page"));
+const updatePagination = useDebounceFn(async () => await setSetting("items_per_page", itemsPerPage.value), 1000);
+
+const flashcardsPerSession = ref(authStore.getSetting<number>("flashcard_per_session"));
+const updateFlashcardsPerSession = useDebounceFn(async () => await setSetting("flashcard_per_session", flashcardsPerSession.value), 1000);
+
+const showSessionIntroduction = ref(authStore.getSetting<boolean>("show_session_introduction"));
+const updateShowSessionIntroduction = useDebounceFn(async () => await setSetting("show_session_introduction", showSessionIntroduction.value), 1000);
+
+const eraseReviews = async () =>
+{
+    modal.open(ModalConfirm, {
+        description: "Are you sure ? This action can not be undone",
+        icon: "i-tabler-alert-triangle",
+        color: "red",
+        async onConfirm()
+        {
+            await repository.flashcard.resetAll();
+
+            useStandardToast("success", {
+                description: "All flashcards have been reset"
+            });
+        }
+    });
+};
 </script>
-
-<style>
-
-</style>
