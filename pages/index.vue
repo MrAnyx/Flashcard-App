39fb567658db68e8a4117842745f60b975f1e6e7
MrAnyx <1ptitigre@gmail.com>
2024-12-28 14:27:54 +0000

    feat(landing-page) redesign landing page layout with navigation and dynamic content

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,17 @@
 <template>
-    <div class="h-svh">
-        <h1>Landing page</h1>
-        <NuxtLink to="/app">
-            App
-        </NuxtLink>
-        <NuxtLink to="/auth/login">
-            Login
-        </NuxtLink>
-        <NuxtLink to="/pricing">
-            Pricing
-        </NuxtLink>
-    </div>
+    <section class="flex flex-col h-svh overflow-auto">
+        <nav class="h-16 w-full fixed flex items-center justify-center border-b border-gray-200 dark:border-gray-800 backdrop-blur">
+            <div class="w-7/12">
+                nav
+            </div>
+        </nav>
+        <div
+            v-for="i in 100"
+            :key="i"
+        >
+            toto
+        </div>
+    </section>
 </template>
 
 <script setup lang="ts">
